2cf9325beb800499b89b22fd1a56911fb91a1671
otogi <devjg7797@naver.com>
2024-12-06 19:34:24 +0900

    뱀 이동 구현

--- SnakeGame/SnakeGame.asm
+++ SnakeGame/SnakeGame.asm
@@ -9,11 +9,14 @@ millseconds     dq 100
 consoleHandle   dq 0
 snakeHeadPos    dd 0
 
-wkeyCode       equ 0x57          
-akeyCode       equ 0x41    
-skeyCode       equ 0x53    
-dkeyCode       equ 0x44      
+wkeyCode        equ 0x57          
+akeyCode        equ 0x41    
+skeyCode        equ 0x53    
+dkeyCode        equ 0x44      
 
+heapHandle      dq 0
+snake           dq 0
+snakeLength     dd 3
 cursorInfo:
    dd 1 
    dd 0 
@@ -37,19 +40,36 @@ extern SetConsoleCursorPosition
 extern GetLastError
 extern system
 extern GetAsyncKeyState
-
+extern GetProcessHeap
+extern HeapAlloc
+extern HeapFree
 main:
    push    rbp
    mov     rbp, rsp
    sub     rsp, 32            
    and     rsp, -16           
 
+   ; 힙 초기화
+   call    GetProcessHeap
+   mov     [heapHandle], rax
+   mov     rcx, [heapHandle]
+   xor     rdx, rdx
+   mov     r8, 1024
+   call    HeapAlloc
+   mov     [snake], rax
+
    ; 플레이어 좌표 초기화
    xor     rax, rax
-   mov     ax,   5
-   mov     word [snakeHeadPos], ax
-   mov     word [snakeHeadPos+2], ax
-
+   mov     rcx, [snake]
+   mov     [rcx], word 5
+   mov     [rcx + 2], word 5
+   add     rcx, 4
+   mov     [rcx], word 4
+   mov     [rcx + 2], word 5
+   add     rcx, 4
+   mov     [rcx], word 3
+   mov     [rcx + 2], word 5
+   
    mov     ecx, -11
    sub     rsp, 32            
    call    GetStdHandle
@@ -70,6 +90,12 @@ GAME_LOOP:
    call     Sleep
    add      rsp, 32
    jmp      GAME_LOOP
+   
+   mov      rcx, [heapHandle]
+   xor      rdx, rdx
+   mov      r8,  [snake]
+   call     HeapFree
+   
    xor      rcx, rcx
    call     ExitProcess
    ret
@@ -84,12 +110,18 @@ update:
    mov      ecx, wkeyCode
    call     GetAsyncKeyState
    add      rsp, 32
-   
+    
+   mov      word [rbp - 4], 4
+   mov      word [rbp - 6], 0
+   mov      word [rbp - 8], 0 
+    
    test    rax, rax
    jz      W_NOT_PRESSED
+
    mov     word [rbp - 4], 0 ; UP   
    mov     word [rbp - 6], 0 ; nextX
    mov     word [rbp - 8], -1 ; nextY
+   
             
 W_NOT_PRESSED:
    
@@ -100,6 +132,7 @@ W_NOT_PRESSED:
    
    test     rax, rax
    jz       A_NOT_PRESSED
+   
    mov      word   [rbp - 4], 1 ; LEFT
    mov      word   [rbp - 6], -1 ; nextX
    mov      word   [rbp - 8], 0 ; nextY
@@ -131,41 +164,102 @@ S_NOT_PRESSED:
    mov      word   [rbp - 8], 0 ; nextY
    
 D_NOT_PRESSED:
-
-
-
-   ; 플레이어 위치 버퍼에 반영
-   xor      rax, rax
-   xor      rbx, rbx
-   mov      bx, word [snakeHeadPos]
-   mov      ax, word [snakeHeadPos+2]
-   imul     eax, [width]
-   add      eax, ebx
+  
    
-   mov      byte [frontBuffer + eax], ' '
-   
-
-   ; 플레이어 좌표 변경
-   xor      rax, rax
-   xor      rbx, rbx
-   mov      bx, word [snakeHeadPos]
-   add      bx, [rbp-6]
-   mov      word [snakeHeadPos], bx
-   mov      ax, word [snakeHeadPos+2]     
-   add      ax, [rbp-8]
-   mov      word [snakeHeadPos+2], ax
-
+   cmp      dword [rbp - 4], 4
+   je       NOT_UPDATE
+  
   
    ; 플레이어 위치 버퍼에 반영
+   mov      dword [rbp-28], 0
+TEMP_FOR_LOOP1:
    xor      rax, rax
    xor      rbx, rbx
-   mov      bx, word [snakeHeadPos]
-   mov      ax, word [snakeHeadPos+2]
-   imul     eax, [width]
-   add      eax, ebx
+   xor      rdx, rdx
+   mov      edx, dword [rbp-28]
+   shl      edx, 2
+   mov      rcx, [snake]
+   add      rcx, rdx
+   mov      ax, word [rcx + 2]
+   imul     ax, [width]
+   mov      bx, word [rcx]
+   add      rax, rbx
+   mov      byte [frontBuffer + rax], ' '
+   add      dword [rbp - 28], 1
+   mov      edx, dword [snakeLength]
+   cmp      dword [rbp - 28], edx
+   jne      TEMP_FOR_LOOP1       
    
-   mov      byte [frontBuffer + eax], 'O'
+   mov       dword [rbp - 14], dword 0 ; prevPos
+   mov       dword [rbp - 18], dword 0 ; nextPos
+   mov       dword [rbp - 24], dword 0 ; 인덱스
    
+   mov      rcx,   [snake]
+   mov      ax, word [rcx]
+   mov      bx, word [rcx + 2]
+   mov      word  [rbp - 14], ax
+   mov      word  [rbp - 16], bx    ; prevPos 세팅
+   
+   xor      rax, rax
+   xor      rbx, rbx
+   
+   mov      rcx, [snake]
+   mov      ax, word [rcx]
+   add      ax, word [rbp - 6]
+   mov      [rbp - 18], word ax
+   mov      bx, word [rcx + 2]
+   add      bx, word [rbp - 8]
+   mov      [rbp - 20], word  bx     ; nextPos 세팅
+   
+TEMP_FOR_LOOP2:
+    xor     rdx, rdx
+    mov     edx, [rbp - 24]     ; Index
+    shl     edx, 2
+    mov     rcx, [snake]        
+    add     rcx, rdx 
+    mov     ax, word [rcx]          ; prevPos 저장
+    mov     word [rbp - 14], ax
+    mov     ax, word [rcx + 2]
+    mov     word [rbp - 16], ax
+
+    mov     ax, word [rbp - 18]     ; nowPos 갱신
+    mov     word [rcx], ax
+    mov     ax, word [rbp - 20]
+    mov     word [rcx + 2], ax
+
+    mov     ax, word [rbp - 14]     ; nextPos 갱신
+    mov     word [rbp - 18], ax
+    mov     ax, word [rbp - 16]
+    mov     word [rbp - 20], ax 
+    
+    xor     rdx, rdx    
+    mov     edx, dword [snakeLength]
+    add     dword [rbp - 24], 1
+    cmp     dword [rbp - 24], edx
+    jne     TEMP_FOR_LOOP2       
+
+
+   ; 플레이어 위치 버퍼에 반영
+   mov      dword [rbp-28], 0
+TEMP_FOR_LOOP3:
+   xor      rax, rax
+   xor      rbx, rbx
+   xor      rdx, rdx
+   mov      edx, dword [rbp-28]
+   shl      edx, 2
+   mov      rcx, [snake]
+   add      rcx, rdx
+   mov      ax, word [rcx + 2]
+   imul     ax, [width]
+   mov      bx, word [rcx]
+   add      rax, rbx
+   mov      byte [frontBuffer + rax], 'O'
+   add      dword [rbp - 28], 1
+   mov      edx, dword [snakeLength]
+   cmp      dword [rbp - 28], edx
+   jne       TEMP_FOR_LOOP3     
+
+NOT_UPDATE:   
    mov      rsp, rbp
    pop      rbp
    ret
